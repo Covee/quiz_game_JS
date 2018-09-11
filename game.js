e2b90c397b00dfe11c1a1a42b89a10cf8d153e98
Covee <uniqtop91@gmail.com>
2018-09-11 23:19:19 +0900

    actually i dont need an id in func Questions()

--- game.js
+++ game.js
@@ -1,6 +1,4 @@
-/*
-CODING CHALLENGE
---- Let's build a fun quiz game in the console! ---
+/*  CODING CHALLENGE    --- Let's build a fun quiz game in the console! ---
 
 1. Build a function constructor called Question to describe a question. A question should include:
     a) question itself
@@ -12,29 +10,32 @@ CODING CHALLENGE
 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
-*/
+
+--- Expert level ---
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+11. Display the score in the console. Use yet another method for this.  */
+
+
 function RP() {
-    function Questions(id, question, choices, correct) {
-        this.id = id;
+    function Questions(question, choices, correct) {
         this.question = question;
         this.choices = choices;
         this.correct = correct;
     }
 
     var Q1 = new Questions(
-        0,
         "is cobee looking good?",
         ["yep", "never"],
         0
     );
     var Q2 = new Questions(
-        1,
         "is react good enough as library?",
         ["yes", "no", "it depends"],
         2
     );
     var Q3 = new Questions(
-        2,
         "the most fuckest thing in korea?",
         ["price", "Housing", "President SalerMoon", "whole world"],
         3
@@ -88,10 +89,3 @@ RP();
 
 
 
-/*
---- Expert level ---
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
-9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
-10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
-11. Display the score in the console. Use yet another method for this.
-*/
